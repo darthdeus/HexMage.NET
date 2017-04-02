82da84ff6278791422e762af40e88750f7f22228
Jakub Arnold <darthdeus@gmail.com>
2017-04-02 15:07:07 +0000

    Update command line flags to reflect EnableGnuPlot->GnuPlot rename

--- HexMage.Benchmarks/run_mcts_evo.sh
+++ HexMage.Benchmarks/run_mcts_evo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup mono ./bin/Release/HexMage.Benchmarks.exe --EnableGnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts1 > /local/darthdeus/mcts-1.txt &
-nohup mono ./bin/Release/HexMage.Benchmarks.exe --EnableGnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts10 > /local/darthdeus/mcts-10.txt &
-nohup mono ./bin/Release/HexMage.Benchmarks.exe --EnableGnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts100 > /local/darthdeus/mcts-100.txt &
-nohup mono ./bin/Release/HexMage.Benchmarks.exe --EnableGnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts1000 > /local/darthdeus/mcts-1000.txt &
+nohup mono ./bin/Release/HexMage.Benchmarks.exe --GnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts1 > /local/darthdeus/mcts-1.txt &
+nohup mono ./bin/Release/HexMage.Benchmarks.exe --GnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts10 > /local/darthdeus/mcts-10.txt &
+nohup mono ./bin/Release/HexMage.Benchmarks.exe --GnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts100 > /local/darthdeus/mcts-100.txt &
+nohup mono ./bin/Release/HexMage.Benchmarks.exe --GnuPlot=false --MctsLogging=false --MctsBenchmark=false --factory=Mcts1000 > /local/darthdeus/mcts-1000.txt &
